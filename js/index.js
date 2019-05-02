d4839e79fa681558303106c2f63a0c7526b6e46c
Kelsy <kelsywells@gmail.com>
2019-05-01 19:38:23 -0700

    Added all the stuff. Just needs layout updates.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
+
 const siteContent = {
+
   "nav": {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
@@ -8,16 +10,22 @@ const siteContent = {
     "nav-item-6": "Contact",
     "img-src": "img/logo.png"
   },
+
   "cta": {
     "h1": "DOM Is Awesome",
     "button": "Get Started",
+
     "img-src": "img/header-img.png"
   },
+
+
   "main-content": {
     "features-h4":"Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "about-h4":"About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    
+
     "middle-img-src": "img/mid-page-accent.jpg",
     "services-h4":"Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
@@ -26,17 +34,55 @@ const siteContent = {
     "vision-h4":"Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
+
+
   "contact": {
     "contact-h4" : "Contact",
     "address" : "123 Way 456 Street Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
+
+
   "footer": {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
 
+
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img\logo.png"])
+
+
+const aTags = document.getElementsByTagName('a');
+  console.log(aTags);
+
+const navTags = document.getElementsByTagName('nav');
+console.log(navTags);
+navTags.style.color= 'green';
+
+const logoImg = document.querySelector('#logo-img');
+  console.log(logoImg);
+  logoImg.setAttribute('src','img\logo.png');
+
+const ctaClass = document.getElementsByClassName('cta');
+console.log(ctaClass);
+
+const mainContent = document.getElementsByClassName('main-content');
+console.log(mainContent);
+
+const headerImg = document.querySelector('#header-img');
+console.log(headerImg);
+headerImg.setAttribute('src', 'img/header-img.png');
+
+const middleImg = document.querySelector('#middle-img');
+console.log(middleImg);
+
+const contactInfo = document.getElementsByClassName('contact');
+console.log(contactInfo);
+
+const footer = document.getElementsByTagName('footer');
+console.log(footer);
+
